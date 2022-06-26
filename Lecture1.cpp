be4228fd862fd935159fdd69a18c57e4df56e90a
agrawalvs <103302374+agrawalvs@users.noreply.github.com>
2022-06-26 20:20:06 +0530

    complete Project

--- Lecture1.cpp
+++
@@ -1,7 +0,0 @@
-# include <windows.h>
-
-int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) // in .c we can use int_stdcall WinMain()
-{
-    MessageBox(0,"hello","Title",MB_OK);
-    return 0;
-}
